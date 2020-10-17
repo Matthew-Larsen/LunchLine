cb1ad83fdbb12801e978aef120ba6b3b510b1c0f
matt.larsen <matt.larsen@mattlarsens-iMac.local>
2020-10-17 10:03:52 -0700

    messing with git

--- LunchLine/LunchLine/ContentView.swift
+++ LunchLine/LunchLine/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                     }
                 }
                 .tag(0)
-            Text("Second View")
+            Text("Third View")
                 .font(.title)
                 .tabItem {
                     VStack {
